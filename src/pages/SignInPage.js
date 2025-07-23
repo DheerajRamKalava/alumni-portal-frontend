1401be422279ed81e68ed56b7909b85dcd410dc0
DheerajRamKalava <dheerajramkalava@gmail.com>
2025-07-23 23:30:21 +0530

    signin bg

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -26,7 +26,8 @@
     };
 
     return (
-      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      // <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="min-h-screen flex items-center justify-center">
         <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-sm">
           <h2 className="text-2xl font-semibold text-center mb-6">Sign In</h2>
 
